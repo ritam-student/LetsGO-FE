f7c97bd84315ba7b3659bb4c391c0eaf23f4d05a
unknown <ritamacharya2004@gmail.com>
2025-04-09 01:37:27 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+  base: "/LetsGO-FE/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libraries into separate chunks
+          react: ['react', 'react-dom'],
+          swiper: ['swiper'],
+        },
+      },
+    },
+  },
 })
